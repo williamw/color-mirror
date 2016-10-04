431b6537b6b94180d3a8f3bfbaf2df8863b1eac5
Bill Welense <bill@polymathic.me>
2016-10-04 17:51:23 -0500

    Further attempt at Generator class

--- color-mirror.js
+++ color-mirror.js
@@ -10,10 +10,10 @@ program
 	.option('-i, --image [path/to/file]', 'specify the image file to extract colors from')
 	.option('-s, --samples [int]', 'specify colors in initial palette (64)')
 	.option('-q, --quality [int]', 'specify scale down factor. 1 means no downsampling (5)')
-	.option('-l, --minNormalLuma [0-1]', 'specify minimal luma threshold for the Vibrant and Muted swatches')
-	.option('-A, --all', 'output all extracted colors (default is most vibrant only)')
-	.option('-h, --hex', 'output color value(s) in Hex')
-	.option('-H, --hsl', 'output color value(s) in HSL')
+	.option('-m, --minVibrantSaturation [0-1]', 'specify minimal saturation threshold (0.35)')
+	.option('-a, --all', 'output all extracted colors (default is most vibrant only)')
+	.option('-#, --hex', 'output color value(s) in Hex')
+	.option('-h, --hsl', 'output color value(s) in HSL')
 	.option('-r, --rgb', 'output color value(s) in RGB')
 	.option('-d, --debug', 'display debug messages')
 	.parse(process.argv)
@@ -23,19 +23,40 @@ if (program.image) {
 	console.log('Getting colors from image %s...', program.image)
 	
 	var optsG = {
-		minNormalLuma: 0.3
+		targetDarkLuma: 0.26,
+		maxDarkLuma: 0.45,
+		
+		minLightLuma: 0.55,
+		targetLightLuma: 0.74,
+		
+		minNormalLuma: 0.3,
+		targetNormalLuma: 0.5,
+		maxNormalLuma: 0.7,
+		
+		targetMutedSaturation: 0.3,
+		maxMutesSaturation: 0.4,
+		
+		targetVibrantSaturation: 1.0,
+		minVibrantSaturation: 0.35,
+		
+		weightSaturation: 3,
+		weightLuma: 6,
+		weightPopulation: 1
 	}
-	if (program.minNormalLuma) optsG.minNormalLuma = program.minNormalLuma
+	if (program.minVibrantSaturation) optsG.minVibrantSaturation = program.minVibrantSaturation
+		
 	var g = new Vibrant.Generator(optsG)
 	if (program.debug) console.log('Created Vibrant Generator')
 	// TO DO: Figure out why the Vibrant instance below doesn't seem to use this generator
 	
 	var optsV = {
 		colorCount: 64,
-		quality: 5
+		quality: 5,
+		useGenerator: g
 	}
 	if (program.samples) optsV.colorCount = program.samples
 	if (program.quality) optsV.quality = program.quality
+		
 	var v = new Vibrant(program.image, optsV)
 	if (program.debug) console.log('Created Vibrant object')
 	
